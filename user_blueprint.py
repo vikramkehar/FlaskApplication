60d16f642458efdc3f5bbd7d8def7ec24b002e39
vikram <vikram@techcompiler.com>
2024-03-23 23:04:54 +0530

    commit4

--- user_blueprint.py
+++ user_blueprint.py
@@ -7,23 +7,30 @@ user_blueprint = Blueprint('user', __name__)
 @user_blueprint.route('/add-user', methods=['POST'])
 def create_user():
     data = request.json
+    print(data)
     username = data.get('username')
     email = data.get('email')
     first_name = data.get('first_name')
     last_name = data.get('last_name')
     date_of_birth = data.get('date_of_birth')
+    projectid = data.get( 'project_name' )
 
     if not username or not email:
         return jsonify({'error': 'Missing required fields'}), 400
 
-    user = User(username=username, email=email, first_name=first_name, last_name=last_name, date_of_birth=date_of_birth)
+    user = User(username=username, email=email, first_name=first_name, last_name=last_name, date_of_birth=date_of_birth,projectid=projectid)
     db.session.add(user)
     db.session.commit()
+
     return jsonify({'message': 'User created successfully'}), 201
 
 @user_blueprint.route('/userlist', methods=['GET'])
 def get_all_users():
     users = User.query.all()
+    projects = Project.query.all()
+    project_data = {}
+    for project in projects:
+        project_data[project.project_id] = project.project_name
     result = []
     for user in users:
         user_data = {
@@ -35,7 +42,7 @@ def get_all_users():
             'date_of_birth': user.date_of_birth.strftime('%Y-%m-%d') if user.date_of_birth else None,
             'created_at': user.created_at.strftime('%Y-%m-%d %H:%M:%S'),
             'updated_at': user.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
-            'project_name': [project.project_name for project in user.projects]
+            'project_name': project_data[user.projectid] if user.projectid else ""
         }
         result.append(user_data)
     return jsonify(result)
